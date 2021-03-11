5c8d846174e50397a6efbb1a337f6e9477f1594f
Wedge Taylor <wtaylor@et-m5.kzn.21-school.ru>
2021-03-11 21:24:53 +0300

    nginx working

--- setup.sh
+++ setup.sh
@@ -1,19 +1,23 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    Dockerfile                                         :+:      :+:    :+:    #
+#    setup.sh                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: wtaylor <wtaylor@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2021/03/07 17:00:41 by wtaylor           #+#    #+#              #
-#    Updated: 2021/03/09 22:38:40 by wtaylor          ###   ########.fr        #
+#    Created: 2021/03/11 20:10:09 by wtaylor           #+#    #+#              #
+#    Updated: 2021/03/11 21:13:29 by wtaylor          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-FROM alpine
+minikube start --vm-driver=virtualbox  --disk-size="10000mb" --memory="4000mb"
 
-RUN apk add nginx
+minikube addons enable metallb
 
-RUN mkdir /run/nginx/
+kubectl apply -f ./srcs/lb.yaml
 
-CMD nginx -g 'daemon off;'
+eval $(minikube docker-env)
+
+docker build -t ft_nginx ./srcs/nginx
+
+kubectl apply -f ./srcs/nginx/nginx.yaml
