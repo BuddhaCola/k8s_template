2dc64de020c3504d404cf5baa7ea25400cf02b50
Wedge Taylor <wtaylor@et-c2.kzn.21-school.ru>
2021-03-19 19:38:09 +0300

    working

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 #    By: wtaylor <wtaylor@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/03/17 19:25:47 by wtaylor           #+#    #+#              #
-#    Updated: 2021/03/19 14:53:39 by wtaylor          ###   ########.fr        #
+#    Updated: 2021/03/19 15:12:23 by wtaylor          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,6 +18,8 @@ minikube addons enable metallb
 kubectl apply -f srcs/configmap.yaml
 
 eval $(minikube docker-env)
+
+docker login
 
 docker build -t nginx srcs/nginx
 
