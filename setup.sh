2765933ef47f82672df3bbc6f50e98c06e4f89f1
Wedge Taylor <wtaylor@si-b3.kzn.21-school.ru>
2021-03-21 21:16:16 +0300

    wtf am I doin

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 #    By: wtaylor <wtaylor@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/03/17 19:25:47 by wtaylor           #+#    #+#              #
-#    Updated: 2021/03/21 14:42:39 by wtaylor          ###   ########.fr        #
+#    Updated: 2021/03/21 18:36:34 by wtaylor          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,20 +15,16 @@ minikube start --vm-driver=virtualbox  --disk-size="10000mb" --memory="4000mb"
 
 minikube addons enable metallb
 
-kubectl apply -f srcs/configmap.yaml
-
 eval $(minikube docker-env)
 
 docker login
 
 docker build -t nginx srcs/nginx
+docker build -t phpmyadmin ./srcs/phpmyadmin
+#docker build -t wordpress srcs/wordpress
 
 kubectl apply -f srcs/nginx/nginx.yaml 
-
-docker build -t php-img ./srcs/phpmyadmin
-
 kubectl apply -f srcs/phpmyadmin/php.yaml
-
-#docker build -t wordpress-img srcs/wordpress
-
 #kubectl apply -f srcs/wordpress/wordpress.yaml
+
+kubectl apply -f srcs/configmap.yaml
