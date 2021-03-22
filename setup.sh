21c01e5acffadb89184879e189231dcbbb37d4a8
Wedge Taylor <wtaylor@si-e1.kzn.21-school.ru>
2021-03-22 16:19:00 +0300

    mysql and wp added

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 #    By: wtaylor <wtaylor@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/03/17 19:25:47 by wtaylor           #+#    #+#              #
-#    Updated: 2021/03/21 18:36:34 by wtaylor          ###   ########.fr        #
+#    Updated: 2021/03/22 15:18:23 by wtaylor          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,10 +21,10 @@ docker login
 
 docker build -t nginx srcs/nginx
 docker build -t phpmyadmin ./srcs/phpmyadmin
-#docker build -t wordpress srcs/wordpress
+docker build -t wordpress-img srcs/wordpress
+docker build -t mysql-img srcs/mysql
 
 kubectl apply -f srcs/nginx/nginx.yaml 
 kubectl apply -f srcs/phpmyadmin/php.yaml
-#kubectl apply -f srcs/wordpress/wordpress.yaml
-
+kubectl apply -f srcs/wordpress/wordpress.yaml
 kubectl apply -f srcs/configmap.yaml
