3294555c5de135f3018207bc7630f104ddc744f9
Wedge Taylor <wtaylor@pr-p3.kzn.21-school.ru>
2021-03-29 21:42:40 +0300

    13

--- srcs/wordpress/wp-config.php
+++ srcs/wordpress/wp-config.php
@@ -2,7 +2,7 @@
 define( 'DB_NAME', 'wordpress' );
 define( 'DB_USER', 'admin' );
 define( 'DB_PASSWORD', 'qwerty' );
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'mysql' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
