c5dd0887b9301c4dacff3f5d28c3e263fbcef49e
Wedge Taylor <wtaylor@un-h2.kzn.21-school.ru>
2021-04-13 15:01:46 +0300

    sql working

--- srcs/mysql/setup.sh
+++ srcs/mysql/setup.sh
@@ -6,21 +6,27 @@
 #    By: wtaylor <wtaylor@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/03/21 19:41:00 by wtaylor           #+#    #+#              #
-#    Updated: 2021/04/09 19:30:33 by wtaylor          ###   ########.fr        #
+#    Updated: 2021/04/13 14:24:43 by wtaylor          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!bin/sh
 openrc default
 
-touch /run/openrc/softlevel
+# touch /run/openrc/softlevel
 
 /etc/init.d/mariadb setup
 
-# cp ./my.cnf /etc/
+sed -i "s|\[mysqld]|\[mysqld]\nskip-networking=false\nbind-address=0.0.0.0|g" /etc/my.cnf
+
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
+
+sed -i "s|.*skip-networking.*|skip-networking=false|g" /etc/my.cnf.d/mariadb-server.cnf
 
 openrc -s mariadb start
 
-mysql < sql_init && rm sql_init && openrc -s mariadb stop
+mysql < sql_init && rm sql_init
+
+openrc -s mariadb stop
 
 /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
